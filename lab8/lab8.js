d9834e9d6b4ec520a6ecdeee72d8e12e2e82517b
lsdamanl <shipiloffdima@ya.ru>
2024-05-25 09:28:49 +0700

    Вывод даты

---
+++ lab8/lab8.js
@@ -0,0 +1,26 @@
+function showDate() {
+    let out = document.getElementById('current-date');
+    let today = new Date();
+    
+    let locales = {
+        'Дата и время для русской локали': 'ru-RU',
+        'Дата и время для локали ЮАР': 'en-ZA',
+        'Дата и время для локали Египта': 'ar-EG',
+        'Дата и время для греческой локали': 'el-GR',
+        'Дата и время для пакистанской локали': 'en-PK',
+        'Дата и время для китайской локали': 'zh-CN',
+        'Дата и время для французской локали': 'fr-FR',
+        'Дата и время для немецкой локали': 'de-DE',
+        'Дата и время для японской локали': 'ja-JP',
+        'Дата и время для индийской локали': 'hi-IN'
+    };
+
+    let result = '';
+
+            for (let [localeName, localeCode] of Object.entries(locales)) {
+                result += `${localeName}: ${today.toLocaleString(localeCode)}<br>`;
+            }
+
+            out.innerHTML = result;
+        
+    }
